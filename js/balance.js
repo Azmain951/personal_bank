14623d629af9605d052dd477a3b9fa3930db55e3
Afridi Azmain <Azmain951>
2022-02-17 01:14:22 +0600

    done and dusted

--- js/balance.js
+++ js/balance.js
@@ -1,14 +1,18 @@
+// function to  get input field value
 function getInputValue(inputId) {
     const costText = document.getElementById(inputId).value;
     const cost = parseFloat(costText);
     return cost;
 }
 
+// function to calculate and update expenses,balance and error handeling
 function updateBalanceFields(income, foodCost, rentCost, clothCost) {
+    // checking string input
     if (isNaN(income) || isNaN(foodCost) || isNaN(rentCost) || isNaN(clothCost)) {
         errorMessage('main-error', 'input a number!!!');
     }
 
+    // checking negative input
     else if (income < 0 || foodCost < 0 || rentCost < 0 || clothCost < 0) {
         errorMessage('main-error', 'enter a positive value!!!');
     }
@@ -30,12 +34,16 @@ function updateBalanceFields(income, foodCost, rentCost, clothCost) {
     }
 }
 
+// function to calculate the saving amount and remaining balance
 function savingAmountCalculation() {
     const savingPercentage = getInputValue('saving-percentage');
     remainingBalanceField = document.getElementById('remaining-balance');
+
+    // checking string input
     if (isNaN(savingPercentage)) {
         errorMessage('saving-error', 'please enter a positive integer value');
     }
+    // checking negative input
     else if (savingPercentage < 0) {
         errorMessage('saving-error', 'please enter a positive integer value');
     }
@@ -47,6 +55,7 @@ function savingAmountCalculation() {
         const balanceText = document.getElementById('balance').innerText;
         const balance = parseFloat(balanceText);
 
+        // checking if saving amount is greater than remaining balance
         if (savingAmount > balance) {
             errorMessage('saving-error', "You don't enough money for saving!!!");
             remainingBalanceField.innerText = '';
@@ -58,18 +67,20 @@ function savingAmountCalculation() {
     }
 }
 
+// function to show error message
 function errorMessage(field, message) {
     const error = document.getElementById(field);
     error.innerText = message;
     error.style.color = "red";
 }
 
+//function to clear error message
 function noErrorMessage(field) {
     const error = document.getElementById(field);
     error.innerText = '';
 }
 
-
+//calculate button event handler
 document.getElementById('calculate-btn').addEventListener('click', function () {
     noErrorMessage('main-error');
     const income = getInputValue('total-income');
@@ -80,6 +91,7 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     updateBalanceFields(income, foodCost, rentCost, clothCost);
 });
 
+//save button event handler
 document.getElementById('save-btn').addEventListener('click', function () {
     noErrorMessage('saving-error');
     savingAmountCalculation();
